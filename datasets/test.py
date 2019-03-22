e2a37179d4dc26802ec432db0a688f2613f4949f
Donggeun <donggeun@Donggeuns-MacBook-Pro.local>
2019-03-22 16:05:09 +0900

    Add Train

--- datasets/test.py
+++ datasets/test.py
@@ -1,19 +1,5 @@
-import numpy as np
-from utils import audio
-from scipy import signal
-from hparams import hparams, hparams_debug_string
-import matplotlib.pyplot as plt
+from text import text_to_sequence
 
-def preemphasis(wav, k, preemphasize=True):
-	if preemphasize:
-		return signal.lfilter([1, -k], [1], wav)
-	return wav
-
-x = audio.load_wav("./kss/1/1_0000.wav", sr=44100)
-mel_spectrogram = audio.melspectrogram(x, hparams).astype(np.float32)
-linear_spectrogram = audio.linearspectrogram(x, hparams).astype(np.float32)
-print(mel_spectrogram)
-print(linear_spectrogram)
-plt.plot(mel_spectrogram)
-#plt.plot(linear_spectrogram)
-plt.show()
+text = "10101010"
+a = text_to_sequence(text)
+print(a)
